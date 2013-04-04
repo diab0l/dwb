83296b0536e9a624d1b49f19d0b8e8420925dc2b
portix <none@none>
2013-04-04 12:17:27 +0200

    Shorter implementation of console methods

--- scripts/lib/console.js
+++ scripts/lib/console.js
@@ -1,95 +1,92 @@
 (function() {
+    /**
+     * @name assert
+     * @memberOf console
+     * @function
+     * @param {Expression} Expression to test
+     * */
+    /**
+     * @name count
+     * @memberOf console
+     * @function
+     * @param {String} Name
+     * */
+    /**
+     * @name debug
+     * @memberOf console
+     * @function
+     * @param {Object} argument Argument passed to real function
+     * */
+    /**
+     * @name error
+     * @memberOf console
+     * @function
+     * @param {Object} argument Argument passed to real function
+     * */
+    /**
+     * @name group
+     * @memberOf console
+     * @function
+     * @param {Object} argument Argument passed to real function
+     * */
+    /**
+     * @name groupCollapsed
+     * @memberOf console
+     * @function
+     * @param {Object} argument Argument passed to real function
+     * */
+    /**
+     * @name groupEnd
+     * @memberOf console
+     * @function
+     * @param {Object} argument Argument passed to real function
+     * */
+    /**
+     * @name info
+     * @memberOf console
+     * @function
+     * @param {Object} argument Argument passed to real function
+     * */
+    /**
+     * @name log
+     * @memberOf console
+     * @function
+     * @param {Object} argument Argument passed to real function
+     * */
+    /**
+     * @name time
+     * @memberOf console
+     * @function
+     * @param {String} name Argument passed to real function
+     * */
+    /**
+     * @name timeEnd
+     * @memberOf console
+     * @function
+     * @param {String} name Argument passed to real function
+     * */
+    /**
+     * @name warn
+     * @memberOf console
+     * @function
+     * @param {Object} argument Argument passed to real function
+     * */
     function execute(f, arg)
     {
         tabs.current.inject("var a=JSON.parse(arguments[0]);console." + f + "(a);", 
             JSON.stringify(arg));
     }
-    Object.defineProperties(console, 
+    var i, method;
+    var o = {};
+    var methods = [ 
+        "assert", "count", "debug", "error", "group", "groupCollapsed", 
+        "groupEnd", "info", "log", "time", "timeEnd", "warn" 
+    ];
+    for (i = methods.length; i>=0; --i)
     {
-        /**
-         * @name assert
-         * @memberOf console
-         * @function
-         * @param {Expression} Expression to test
-         * */
-        "assert" : { value : execute.bind(null, "assert") },
-        /**
-         * @name count
-         * @memberOf console
-         * @function
-         * @param {String} Name
-         * */
-        "count" : { value : execute.bind(null, "count") },
-        /**
-         * @name debug
-         * @memberOf console
-         * @function
-         * @param {Object} argument Argument passed to real function
-         * */
-        "debug" : { value : execute.bind(null, "debug") },
-        /**
-         * @name error
-         * @memberOf console
-         * @function
-         * @param {Object} argument Argument passed to real function
-         * */
-        "error" : { value : execute.bind(null, "error") },
-        /**
-         * @name group
-         * @memberOf console
-         * @function
-         * @param {Object} argument Argument passed to real function
-         * */
-        "group" : { value : execute.bind(null, "group") },
-        /**
-         * @name groupCollapsed
-         * @memberOf console
-         * @function
-         * @param {Object} argument Argument passed to real function
-         * */
-        "groupCollapsed" : { value : execute.bind(null, "groupCollapsed") },
-        /**
-         * @name groupEnd
-         * @memberOf console
-         * @function
-         * @param {Object} argument Argument passed to real function
-         * */
-        "groupEnd" : { value : execute.bind(null, "groupEnd") },
-        /**
-         * @name info
-         * @memberOf console
-         * @function
-         * @param {Object} argument Argument passed to real function
-         * */
-        "info" : { value : execute.bind(null, "info") },
-        /**
-         * @name log
-         * @memberOf console
-         * @function
-         * @param {Object} argument Argument passed to real function
-         * */
-        "log" : { value : execute.bind(null, "log") },
-        /**
-         * @name time
-         * @memberOf console
-         * @function
-         * @param {Object} argument Argument passed to real function
-         * */
-        "time" : { value : execute.bind(null, "time") },
-        /**
-         * @name timeEnd
-         * @memberOf console
-         * @function
-         * @param {String} name Argument passed to real function
-         * */
-        "timeEnd" : { value : execute.bind(null, "timeEnd") },
-        /**
-         * @name warn
-         * @memberOf console
-         * @function
-         * @param {String} name Argument passed to real function
-         * */
-        "warn" : { value : execute.bind(null, "warn") }
-    });
+        method = methods[i];
+        o[method] = { value : execute.bind(null, method) };
+    }
+    Object.defineProperties(console, o);
     Object.freeze(console);
 })();
