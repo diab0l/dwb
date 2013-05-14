b0efd96352f031eef5c031cb601fb8594f7d3e7e
portix <portix@gmx.net>
2013-05-14 16:42:49 +0200

    Replace hash-signs in download paths

--- src/util.c
+++ src/util.c
@@ -387,7 +387,16 @@ util_normalize_filename(char *buffer, const char *filename, size_t length)
     char *tmp = buffer;
     for (size_t i=0; *filename != 0 && i<length-1; i++, filename++, buffer++) 
     {
-        *buffer = *filename == '/' ? '_' : *filename;
+        switch (*filename) 
+        {
+            case '/':
+            case '#':
+                *buffer = '_';
+                break;
+            default : 
+                *buffer = *filename;
+                break;
+        }
     }
     *buffer = 0;
     return tmp;
