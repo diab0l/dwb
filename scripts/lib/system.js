9aad00aba6bf44a652b8f85b23f63952b4c3b161
portix <portix@gmx.net>
2013-10-24 17:42:19 +0200

    Return a new Deferred from system.spawn

--- scripts/lib/system.js
+++ scripts/lib/system.js
@@ -5,20 +5,19 @@
             value : (function() {
                 return function(command, onStdout, onStderr, stdin, environ) {
                     var stdout, stderr;
-                    return system._spawn(command, 
-                                !onStdout ? null : function(response) {
-                                    var ret;
-                                    stdout = onStdout.call(onStdout, response) || response;
-                                }, 
-                                !onStderr ? null : function(response) {
-                                    var ret;
-                                    stderr = onStderr.call(onStderr, response) || response;
-                                    return ret;
-                                }, 
-                                stdin, environ).then(
-                                    function() { return stdout; }, 
-                                    function() { return stdin; }
-                                );
+                    var d = new Deferred();
+                    system._spawn(command, 
+                        function(response) {
+                            stdout = onStdout ? (onStdout.call(onStdout, response) || response) : response;
+                        },  
+                        onStderr || function(response) {
+                            stderr = onStderr ? (onStderr.call(onStderr, response) || response) : response;
+                        }, 
+                        stdin, environ).then(
+                            function() { return d.resolve(stdout); }, 
+                            function(status) { return d.reject({ status : status, stderr : stderr }, stderr); }
+                        );
+                    return d;
                 };
             })()
         }
