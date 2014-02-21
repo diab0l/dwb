734290fe9659ae97c897664da4bda05336e946ee
portix <portix@gmx.net>
2014-02-21 09:39:21 +0100

    Cleaning examples directory

---
+++ examples/js/autoreload.js
@@ -0,0 +1,82 @@
+//!javascript
+//
+// Allows to autoreload a tab, adds four new commands
+//  
+//  :[n]ar [s] 
+//      reloads the n-th or current tab if n is omitted using interval s or
+//      defaultInterval if s is omitted
+//
+//  :[n]arstop 
+//      stops autoreloading the nth or current tab
+//
+//  :arall [s]
+//      autoreloads all open tabs using interval s or defaultInterval
+//      newly created tabs will automatically reload too
+//  :arstopall
+//      stops autoreloading of all tabs
+
+// default reload interval
+var defaultInterval = 60; 
+
+// used for 'arall', if set to 0 all tabs will be reloaded at once which may
+// block dwb  with many tabs open
+var reloadDelay = 5;
+
+// bind("shortcut", func, "commandline");
+bind(null, start,       "ar");
+bind(null, stop,        "arstop");
+bind(null, startAll,    "arall");
+bind(null, stopAll,     "arstopall");
+
+
+var onCreateTab = Signal("createTab");
+
+function doStop(wv) {
+    var id = script.deletePrivate(wv, "refresh");
+    if (id) {
+        timer.stop(id);
+    }
+}
+
+function doStart(interval, wv) {
+    doStop(wv);
+    var id = timer.start(interval * 1000, function() {
+        wv.reload();
+    });
+    script.setPrivate(wv, "refresh", id);
+}
+
+function start(cmd) {
+    doStart(parseInt(cmd.arg) || defaultInterval, tabs[cmd.nummod-1] || tabs.current);
+}
+
+function stop(cmd) {
+    doStop(tabs[cmd.nummod-1] || tabs.current);
+}
+
+function startAll(cmd) {
+    var lstart = doStart.bind(null, parseInt(cmd.arg) || defaultInterval);
+    if (reloadDelay > 0) {
+        var delay = 0;
+        tabs.forEach(function(wv) {
+            timer.start(delay * 1000, function() {
+                lstart(wv);
+                return false;
+            });
+            delay += reloadDelay;
+        });
+
+    }
+    else {
+        tabs.forEach(lstart);
+    }
+    onCreateTab.connect(lstart);
+}
+
+function stopAll(cmd) {
+    onCreateTab.disconnect();
+    tabs.forEach(doStop);
+}
+
+Signal.connect("closeTab", doStop);
+
---
+++ examples/js/menusearch.js
@@ -0,0 +1,66 @@
+//!javascript
+//
+// Adds new menus to the right click context menu if text is selected on a
+// website. Add a menu entry for each configured searchengine that can be used
+// to search for the selected text.
+
+// The maximum length of the preview string
+var previewLength = 50;
+
+
+var engines, menuItems;
+
+engines = data.parse("searchEngines").sort(function(a, b) {
+    return a.keyword > b.keyword ? 1 : -1;
+});
+
+__assert__(engines.length !== 0);
+
+menuItems = [ { 
+        label : "_Search", 
+        items : [{}, null].concat(engines.map(engine2item.bind(null, "open"))) 
+    }, { 
+        label : "Search in new _tab", 
+        items : [{}, null].concat(engines.map(engine2item.bind(null, "tabopen"))) 
+    }, { 
+        label : "Search in _background tab", 
+        items : [{}, null].concat(engines.map(engine2item.bind(null, "backopen"))) 
+    }
+];
+
+function engine2item(command, engine) {
+    return {
+        label : "_" + engine.keyword + "   " + engine.host,
+        command : command, 
+        keyword : engine.keyword
+    };
+}
+
+function getSubmenu(label, selection, subitem) {
+    var submenu = new GtkWidget("GtkMenu");
+
+    subitem.items[0].label = label;
+    subitem.items.slice(2).forEach(function(item) {
+        item.callback = execute.bind(null, item.command + " " +  item.keyword + " " + selection);
+    });
+    submenu.addItems(subitem.items);
+
+    return { label : subitem.label, menu : submenu };
+}
+
+Signal.connect("contextMenu", function (wv, menu) {
+    var preview, label, selection;
+
+    selection = util.getSelection();
+    if (!selection) {
+        return;
+    }
+
+    preview = selection;
+    if (previewLength > 0 && preview.length > previewLength) {
+        preview = preview.replace(/\s+/g, " ").substring(0, previewLength - 3) + "...";
+    }
+    label = "Search for: " + preview;
+
+    menu.addItems(menuItems.map(getSubmenu.bind(this, label, selection)));
+});
